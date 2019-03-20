c237438a794ca6fe6f7c64791934036a5a6a360b
arnavnegistudy <arnav_negi@yahoo.com>
2019-03-20 16:58:51 +0530

    rolled back local 1-webpack-starter-app to make upto date with remote 1-webpack-starter-app

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: './src/js/app.js',
-    /*devServer: {
-        contentBase: '/dist'
-    },*/
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist'
+        path: path.resolve(__dirname, 'dist')
     }
 };
