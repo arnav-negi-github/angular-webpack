58c076f151b0614550ce1a83c8bd4b3859f86846
arnavnegistudy <arnav_negi@yahoo.com>
2019-03-20 11:52:34 +0530

    committed after webpack and webpack-dev-server starter app run success with webpack.config.js addition

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/js/app.js',
+    /*devServer: {
+        contentBase: '/dist'
+    },*/
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist'
     }
 };
